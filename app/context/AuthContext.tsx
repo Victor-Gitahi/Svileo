5605fb87e1ed97899bb2c4ac36e435030eff2d20
Victor-Gitahi <wilfredeveloper@gmail.com>
2023-05-26 20:35:16 +0300

    ✨ SIgn Out Functionality Added.

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -1,7 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { onAuthStateChanged, getAuth } from "firebase/auth";
 import firebase_app from "@/app/firebase/config";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Loader from "@/app/components/Loader/Loader";
 import styles from "./styles.module.css";
 
@@ -14,13 +16,28 @@ export const useAuthContext = () => React.useContext(AuthContext);
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = React.useState(null);
   const [loading, setLoading] = React.useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   React.useEffect(() => {
+    const id = toast.loading("Please wait while we verify your Auth status");
+
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
+        setIsLoggedIn(true);
+        toast.update(id, {
+          render: "Signed In",
+          type: "success",
+          isLoading: false,
+        });
       } else {
+        toast.update(id, {
+          render: "Redirecting to Auth",
+          type: "info",
+          isLoading: false,
+        });
         setUser(null);
+        setIsLoggedIn(false);
       }
       setLoading(false);
     });
@@ -29,14 +46,14 @@ export const AuthContextProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, isLoggedIn }}>
       {loading ? (
         <main className={styles.main}>
           <div className={styles.text_content}>
             <h5>Welcome to Svileo</h5>
             <p>Please wait while we verify your Authentication Status</p>
           </div>
-          <Loader />
+          <ToastContainer />
         </main>
       ) : (
         children
