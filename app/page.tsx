560cf952d28882c3c39579a2af8143adcbc5f96f
Victor-Gitahi <wilfredeveloper@gmail.com>
2023-05-26 20:31:33 +0300

    ♻️ Nav component moved into AuthContextProvider
    + To allow dynamic rendering of CTA buttons for registration & Login

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import Graph from "./components/Graph/Graph";
 
 export default function Home() {
   return (
     <>
-      <nav className={styles.nav}>
-        <Link href={"/auth"}>Create Account</Link>
-        <Link href={"/auth/login"}>Login</Link>
-      </nav>
-
       <main className={styles.main}>
-        <h1>SVILEO</h1>
+        <Graph />
       </main>
     </>
   );
